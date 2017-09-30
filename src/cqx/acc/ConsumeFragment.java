71077d6fc8e154757007cbcfcfe0b1f513cb3897
chenqixu <13509323824@139.com>
2017-09-30 22:44:24 +0800

    修改my_card展现地方，修改按钮位置，增加信用卡位置

--- src/cqx/acc/ConsumeFragment.java
+++ src/cqx/acc/ConsumeFragment.java
@@ -216,8 +216,8 @@ import cqx.acc.util.thread.ConsumeThread;
         List<Map<String, Object>> list = new ArrayList<Map<String, Object>>(); 
         for(AccCountsDailyBean bean : Constants.acccountsdailyList){
             Map<String, Object> map = new HashMap<String, Object>();
-            map.put("title", bean.getAcc_type_desc()+" "+bean.getAcc_value()+" "+bean.getAcc_card_name());
-            map.put("info", bean.getAcc_desc());
+            map.put("title", bean.getAcc_type_desc()+" "+bean.getAcc_value());
+            map.put("info", bean.getAcc_desc()+"["+bean.getAcc_card_name()+"]");
             map.put("datevalue", bean.getAcc_use_time());
             list.add(map);
         }         
